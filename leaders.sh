a1e5f40c9ae5fb89d1fb5424da2bd5b1b2942da1
SureshU <sureshdevops@gmail.com>
2025-09-19 07:32:55 +0530

    this is 3rd commit

---
+++ leaders.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo “person1: Hi , How are you Putin”
+echo “person2:I am good, what about you”
+echo “person1: What are you doing trump”
+echo “person2: Nothing putin”
---
+++ v.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1=suresh
+person2=ramesh
+echo "$person1: welcome back"
+echo "$person2: how are you"
+echo "$person1: I am good"
+echo "$person2: what about you"
